292ee7a670a5094941e4f9c2ab0b18ee41fc1794
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-03-05 11:40:49 -0500

    Done.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -69,29 +69,20 @@ describe 'Movie' do
         expect(Movie.find_by(attributes)).to eq(movie)
       end
 
-      it 'can be created in a block when no args are passed' do
-        movie = can_be_created_in_a_block
-
-        expect(movie.title).to eq("Home Alone")
-        expect(movie.release_date).to eq(1990)
-      end
-
       it 'can be created in a block' do
         movie = can_be_created_in_a_block
-        args = { title: "The Room", release_date: 2003 }
-        movie = can_be_created_in_a_block(args)
 
-        expect(movie.title).to eq("The Room")
-        expect(movie.release_date).to eq(2003)
+        expect(Movie.last).to eq(movie)
+        expect(Movie.last.title).to eq("Home Alone")
+        expect(Movie.last.release_date).to eq(1990)
       end
-   end
+    end
 
     context 'reading' do
       before do
         5.times do |i|
           Movie.create(title: "Movie_#{i}", release_date: i+2000)
         end
-        Movie.last.update_attribute(:id, 999)
       end
 
       it 'can get the first item in the database' do
